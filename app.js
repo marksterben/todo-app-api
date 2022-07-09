58c7d1bbaf4c9009c744390ed2e9ed935db1dc96
Marcellino <68014273+marksterben@users.noreply.github.com>
2022-07-09 12:51:53 +0700

    change env var

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const router = require("./routes");
 const { sequelize } = require("./models");
 
 const app = express();
-const port = process.env.APP_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
